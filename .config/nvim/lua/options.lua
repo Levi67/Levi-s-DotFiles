a14191d64f8bc72a6dc26107f8c1021a0092bc5a
Levi Holzner <levisuper@gmx.de>
2025-08-06 22:32:23 +0200

    NVchad with rust support now

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,16 +1,6 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.termguicolors = true
-vim.opt.mouse = "a"
-vim.g.mapleader = " "
---vim.opt.virtualedit = "all" -- For flying through the docs - Going into empty Spacevim
---vim.opt.scrolloff = 999     -- -"-
-vim.opt.clipboard = "unnamedplus" -- For clipboard sharing
+require "nvchad.options"
 
+-- add yours here!
 
-
--- Basic keymap example
-vim.keymap.set("n", "<leader>w", ":w<CR>")  -- Save with <Space>w
+-- local o = vim.o
+-- o.cursorlineopt ='both' -- to enable cursorline!
---
+++ .config/nvim/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    // Print a greeting
+    println!("Hello, Rust LSP!");
+
+    // Simple variable and arithmetic
+    let x = 5;
+    let y = 10;
+    let sum = x + y;
+
+    println!("Sum of {} and {} is {}", x, y, sum);
+
+    println!("hello world");
+    if x >= y {
+        println!("x is larger than y");
+    }
+}
